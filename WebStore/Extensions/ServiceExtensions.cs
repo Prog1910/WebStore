fd4303febf0d0151542caa1c8082acec7f13af43
Prog <shirali.salimov.2002@gmail.com>
2024-06-23 01:14:09 +0400

    refactor: setup logging configuration

--- WebStore/Extensions/ServiceExtensions.cs
+++ WebStore/Extensions/ServiceExtensions.cs
@@ -1,13 +1,17 @@
 ﻿using Contracts;
 using LoggerService;
 using Microsoft.OpenApi.Models;
+using NLog;
 
 namespace WebStore.Extensions;
 
 public static class ServiceExtensions
 {
     public static void ConfigureLogging(this IServiceCollection services)
-        => services.AddSingleton<ILoggerManager, LoggerManager>();
+    {
+        LogManager.Setup().LoadConfigurationFromFile("Config/nlog.config");
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+    }
 
     public static void ConfigureSwagger(this IServiceCollection services)
         => services.AddSwaggerGen(opts
