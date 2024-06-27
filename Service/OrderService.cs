a151f7b4385876506021ae094e5cf52ff0e2b195
Prog <shirali.salimov.2002@gmail.com>
2024-06-27 14:49:48 +0400

    feat: add service manager

---
+++ Service/OrderService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+public class OrderService : IOrderService
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+
+    public OrderService(IRepositoryManager repository, ILoggerManager logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+}
---
+++ Service/ServiceManager.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+public sealed class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IOrderService> _orderService;
+
+    private readonly Lazy<IProductService> _productService;
+
+    public ServiceManager(IRepositoryManager repository, ILoggerManager logger)
+    {
+        _orderService = new Lazy<IOrderService>(() => new OrderService(repository, logger));
+        _productService = new Lazy<IProductService>(() => new ProductService(repository, logger));
+    }
+
+    public IOrderService OrderService => _orderService.Value;
+
+    public IProductService ProductService => _productService.Value;
+}
